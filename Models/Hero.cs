c0471c0ee279f42d887038ef6d9f0623c2b26ed2
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-04-22 08:54:54 +0300

    Step 3: JSON configured - camelCase, enum as string

--- Models/Hero.cs
+++ Models/Hero.cs
@@ -22,6 +22,6 @@ public class Hero
     public List<string> Powers { get; set; } = new();
     public Weapon Weapon { get; set; } = new();
     [JsonIgnore]
-    public static? InternalNotes { get; set; } 
-    
+    public string? InternalNotes { get; set; } 
+
 }
